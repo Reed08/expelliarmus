e7adb69fa50fd608b6a1f176f68ffbde1177f24b
Lincoln Farkas <lincoln.farkas@gmail.com>
2024-06-17 16:27:17 -0400

    Add command to reset guild preferences

---
+++ bot/commands/preferences/reset-preferences.js
@@ -0,0 +1,82 @@
+const {
+	SlashCommandBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+} = require('discord.js');
+const Guild = require('../../../db/models/guild.js');
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('reset-preferences')
+		.setDescription('Resets the preferences for this guild to default values.'),
+	async execute(interaction) {
+		const confirmation = await interaction.reply({
+			content: 'Are you sure you want to reset preferences for this guild?',
+			components: [
+				new ActionRowBuilder().addComponents(
+					new ButtonBuilder()
+						.setCustomId('yes')
+						.setLabel('Yes')
+						.setStyle('Success'),
+					new ButtonBuilder()
+						.setCustomId('no')
+						.setLabel('No')
+						.setStyle('Danger')
+				),
+			],
+		});
+
+		let replied = false;
+
+		const filter = (i) => i.user.id === interaction.user.id;
+		const collector = confirmation.createMessageComponentCollector({
+			filter,
+			time: 15000,
+		});
+
+		collector.on('collect', async (i) => {
+			if (i.customId === 'yes') {
+				const guild = await Guild.findOne({
+					where: { guildId: interaction.guild.id },
+				});
+
+				console.log(guild.spamDetectionStrictness);
+
+				if (
+					guild &&
+					(guild.spamDetectionStrictness !== 1000 || guild.spamPenalty !== 60)
+				) {
+					await guild.update({
+						spamDetectionStrictness: 1000,
+						spamPenalty: 60,
+					});
+					await interaction.editReply({
+						content: `Successfully reset preferences for **${interaction.guild.name}** to default values!`,
+						components: [],
+					});
+				} else {
+					await interaction.editReply({
+						content: `No preferences to reset for **${interaction.guild.name}**!`,
+						components: [],
+					});
+				}
+
+				replied = true;
+			} else {
+				await interaction.editReply({
+					content: 'Reset preferences cancelled.',
+					components: [],
+				});
+
+				replied = true;
+			}
+		});
+
+		collector.on('end', async (collected, reason) => {
+			if (reason === 'time' && !replied) {
+				await interaction.editReply('Reset preferences timed out.');
+			}
+		});
+	},
+};
