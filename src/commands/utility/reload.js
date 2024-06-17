b4fe08946df5dbfc359783e20670a69125a0d4c8
Lincoln Farkas <lincoln.farkas@gmail.com>
2024-06-17 14:40:03 -0400

    Initial commit

---
+++ src/commands/utility/reload.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	cooldown: 5,
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command.')
+		.addStringOption((option) =>
+			option
+				.setName('command')
+				.setDescription('The command to reload.')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		if (interaction.user.id !== '531286082831253515') {
+			return interaction.reply(
+				'You do not have permission to use this command!'
+			);
+		}
+
+		const commandName = interaction.options
+			.getString('command', true)
+			.toLowerCase();
+		const command = interaction.client.commands.get(commandName);
+
+		if (!command) {
+			return interaction.reply(
+				`There is no command with name \`${commandName}\`!`
+			);
+		}
+
+		delete require.cache[require.resolve(`./${command.data.name}.js`)];
+
+		try {
+			const newCommand = require(`./${command.data.name}.js`);
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(
+				`Command \`${newCommand.data.name}\` was reloaded!`
+			);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply(
+				`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``
+			);
+		}
+	},
+};
